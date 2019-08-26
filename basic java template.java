f8efbad5e7d36a6a3b7d88349ea639a79708b9c7
baileydauterman <50151523+baileydauterman@users.noreply.github.com>
2019-08-26 13:23:25 -0400

    Create basic java template.java

---
+++ basic java template.java
@@ -0,0 +1,15 @@
+/*
+Add comments here
+*/
+
+import java.util.*;
+import java.io.PrintStream;
+
+
+class Main {
+  static final PrintStream out = System.out;
+  static final Scanner in = new Scanner(System.in);
+  public static void main(String[] args) {
+
+  }
+}
