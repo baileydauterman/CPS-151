4e76a22c05ef382ba5f45d48e8e3cd39ea66d464
baileydauterman <50151523+baileydauterman@users.noreply.github.com>
2019-09-13 11:07:06 -0400

    Update comboLock.java

--- Programming Assignments/1/comboLock.java
+++ Programming Assignments/1/comboLock.java
@@ -1,8 +1,9 @@
 import java.util.*;
 import java.io.PrintStream;
 
+//define comboLock class
 class comboLock {
-
+  //define variables for comboLock
   static final PrintStream out = System.out;
   static final Scanner in = new Scanner(System.in);
 
@@ -12,24 +13,29 @@ class comboLock {
   int dial = 0;
   int totalNumbers = 40;
 
+  //resets the dial to the top of the lock
   void reset() {
     dial = 0;
   }
 
+  //sets the first part of the combolock
   void setSecret1(int s) {
     secret1 = s;
   }
 
+  //sets the second part of the combolock
   void setSecret2(int s) {
     secret2 = s;
   }
 
+  //sets the third part of the combolock
   void setSecret3(int s) {
     secret3 = s;
   }
 
+  //sets the dial back to 0 and then maths for the amount of ticks in a turn
   void turnLeft(int ticks) {
-    dial = 0;
+    this.reset();
     if(dial - ticks >= 0) {
       dial = dial - ticks;
     } else {
@@ -37,8 +43,9 @@ class comboLock {
     }
   }
 
+  //sets the dial back to 0 and then maths for the amount of ticks in a turn
   void turnRight(int ticks) {
-    dial = 0;
+    this.reset();
     if(dial + ticks < totalNumbers) {
       dial += ticks;
     } else {
@@ -46,6 +53,7 @@ class comboLock {
     }
   }
 
+  //checks to see if the lock has been cracked, calls on the 3 boolean values and then gets user input from the getInput method uses if statement to set and check if the code has been cracked
   boolean isOpen() {
     boolean s1 = false;
     boolean s2 = false;
@@ -76,6 +84,7 @@ class comboLock {
     }
   }
 
+  //displays a prompt box for the user to enter their guess for the lock
   int getInput(String s) {
     int guess;
     do {
